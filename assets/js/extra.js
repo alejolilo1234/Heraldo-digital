e84b4770123fd3523a476095283a02e573133d4d
Alejolilo1234 <65469933+alejolilo1234@users.noreply.github.com>
2021-12-02 03:04:15 +0000

    Initial commit

---
+++ assets/js/extra.js
@@ -0,0 +1,26 @@
+// Change these settings
+
+let pageTitle = 'El Heraldo de la verdad';
+let divText = '|';
+let pageText = 'Página';
+
+FlipbookSettings = {
+  options: {
+    pageWidth: 1115,
+    pageHeight: 1443,
+    pages: 65
+  },
+  shareMessage: pageTitle,
+  pageFolder: 'content/'+pageTitle+'/',
+  loadRegions: true,
+  loadHTML: true
+};
+
+$("#flipbook").bind("turning", function(event, page, view) {
+  window.document.title = pageText+' '+ view.join(" y ") + ' ' + divText + ' ' + pageTitle;
+});
+
+document.addEventListener("DOMContentLoaded",() => {
+  let viewBook = $("#flipbook").turn("view").join(" y ");
+  window.document.title = pageText+' '+ viewBook + ' ' +divText + ' ' + pageTitle;
+})
